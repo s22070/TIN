17b5cc9607b1f12abc4dc881595cba89099b5c6b
Kamil Ossowski <s22070@pjwstk.edu.pl>
2023-06-16 17:00:39 +0200

    added fifth task

---
+++ Zadanie5/5a.js
@@ -0,0 +1,21 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/home') {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end("Welcome to the home page");
+    } else if (req.url === '/register') {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end("This is a page for registration purposes");
+    } else if (req.url === '/cat') {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end("No cat for you!");
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end('Page not supported, yet!');
+    }
+});
+
+server.listen(8080, () => {
+    console.log('Server running');
+});
---
+++ Zadanie5/5b.js
@@ -0,0 +1,37 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/home') {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end(homePage());
+    } else if (req.url === '/register') {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end(registrationPage());
+    } else if (req.url === '/cat') {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end(catPage());
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end(notSupportedPage());
+    }
+});
+
+server.listen(8080, () => {
+    console.log('Server running');
+});
+
+function homePage() {
+    return 'Welcome to the home page';
+}
+
+function registrationPage() {
+    return 'This is a page for registration purposes';
+}
+
+function catPage() {
+    return 'No cat for you!';
+}
+
+function notSupportedPage() {
+    return 'Page not supported, yet!';
+}
